23511f46e63ffd891025cdbb61b823473fd7f271
afcuttin <afcuttin@gmail.com>
2015-04-27 15:18:54 +0200

    successive interference cancellation now working, with elimination of acked twin packets

--- crdsa.m
+++ crdsa.m
@@ -51,22 +51,26 @@ while currentRAF < simulationTime
 % now let's do the successive interference cancellation
 ackedPacketIdx = 1;
 while ackedPacketIdx < numel(acked_col)
+
     twinCol = randomAccessFrame(acked_row(ackedPacketIdx),acked_col(ackedPacketIdx)) % get twin packet slot id
     randomAccessFrame % only for debug
     acked_col % only for debug
     acked_row % only for debug
-    if sum(randomAccessFrame(:,twinCol)>0) ~= 1 % check if twin packet has not collided
+
+    if sum(randomAccessFrame(:,twinCol)>0) > 1 % twin packet has collided
         randomAccessFrame(acked_row(ackedPacketIdx),twinCol) = 0 % cancel twin packet, thus reducing potential interference
         if sum(randomAccessFrame(:,twinCol)>0) == 1 % check if a new package can be acknowledged, thanks to interference cancellation
             acked_col(numel(acked_col) + 1) = twinCol
             acked_row(numel(acked_row) + 1) = find(randomAccessFrame(:,twinCol))
         end
-    % elseif sum(randomAccessFrame(:,twinCol)>0) == 1 % twin packet has not collided
-    %     twinAckedCol = find()
-
-        
+    elseif sum(randomAccessFrame(:,twinCol)>0) == 1 % twin packet has not collided, so let's remove its column index from the acked packets arrays
+        twinAckedInd = find(acked_col == twinCol)
+        acked_col(twinAckedInd) = []
+        acked_row(twinAckedInd) = []
     end
+
     pause % only for debug
+
     ackedPacketIdx = ackedPacketIdx + 1
 end    
 
