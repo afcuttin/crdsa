f95a4d4f88585a53af6589360573e8c4e4f22353
afcuttin <afcuttin@gmail.com>
2015-04-28 19:40:19 +0200

    started to evaluate number of successes and collisions

--- crdsa.m
+++ crdsa.m
@@ -1,9 +1,9 @@
 clear all;
 
-sourceNumber = 15;
+sourceNumber = 10;
 randomAccessFrameLength = 15;
 simulationTime = 1; % total number of RAF
-packetReadyProb = .5;
+packetReadyProb = 1;
 maxBackoff = 5;
 
 sourceStatus = zeros(1,sourceNumber);
@@ -11,57 +11,52 @@ sourceStatus = zeros(1,sourceNumber);
 % 0: source has no packet ready to be transmitted (is idle)
 % 1: source has a packet ready to be transmitted, either because new data must be sent or a previously collided packet has waited the backoff time
 % >1: source is backlogged due to previous packets collision, the value of the status equals the number of slots it must wait for the next transmission attempt
-randomAccessFrame = zeros(sourceNumber,randomAccessFrameLength);
-randomAccessFrameLog = zeros(sourceNumber,randomAccessFrameLength);
-% later on referred to as RAF: Random Access Frame
-currentRAF = 0;
+randomAccessFrame = zeros(sourceNumber,randomAccessFrameLength); % later on referred to as RAF
+% randomAccessFrameLog = zeros(sourceNumber,randomAccessFrameLength);
 
+currentRAF = 0;
 while currentRAF < simulationTime
     currentRAF = currentRAF + 1;
 
-    for eachSource1 = 1:sourceNumber
+    for eachSource1 = 1:sourceNumber % create the RAF
         if sourceStatus(1,eachSource1) == 0 && rand(1) <= packetReadyProb % new packet
             sourceStatus(1,eachSource1) = 1;
             sourceBackoff(1,eachSource1) = randi(maxBackoff,1);
             pcktGenerationTimestamp(1,eachSource1) = currentRAF;
 
-            firstReplicaSlot = randi(randomAccessFrameLength)
-            secondReplicaSlot = randi(randomAccessFrameLength)
+            firstReplicaSlot = randi(randomAccessFrameLength);
+            secondReplicaSlot = randi(randomAccessFrameLength);
             while secondReplicaSlot == firstReplicaSlot
-            	secondReplicaSlot = randi(randomAccessFrameLength)
+            	secondReplicaSlot = randi(randomAccessFrameLength);
         	end
             randomAccessFrame(eachSource1,firstReplicaSlot) = secondReplicaSlot;
             randomAccessFrame(eachSource1,secondReplicaSlot) = firstReplicaSlot;
-
         elseif sourceStatus(1,eachSource1) == 1 % backlogged packet
             sourceBackoff(1,eachSource1) = randi(maxBackoff,1);
         end
     end
-    randomAccessFrame
 
-    acked_col=find(sum(randomAccessFrame>0)==1) % find column indexes of packets without collisions
-    
+    acked_col=find(sum(randomAccessFrame>0)==1); % find column indices of packets without collisions
     [row_c,col_c,twinSlotId] = find(randomAccessFrame);
-        row=transpose(row_c);
+    row=transpose(row_c);
     col=transpose(col_c);
     [~,col_ind]=ismember(acked_col,col);
+    acked_row = row(col_ind); % find row indices of packets without collisions
 
-    acked_row = row(col_ind)
-
-[sicRAF,sicCol,sicRow] = sic(randomAccessFrame,acked_col,acked_row);
+    [sicRAF,sicCol,sicRow] = sic(randomAccessFrame,acked_col,acked_row); % do the Successive Interference Cancellation
 
 sicRAF
 sicCol
 sicRow
-    
 
-    % pcktTransmissionAttempts = pcktTransmissionAttempts + sum(sourceStatus == 1);
-
+    pcktTransmissionAttempts = pcktTransmissionAttempts + sum(sourceStatus == 1);
+    ackdPacketCount = ackdPacketCount + numel(sicCol);
     % if sum(sourceStatus == 1) == 1
-    %     ackdPacketCount = ackdPacketCount + 1;
+    if numel(sicCol) > 0
+
     %     [~,sourceId] = find(sourceStatus == 1);
     %     ackdPacketDelay(ackdPacketCount) = currentRAF - pcktGenerationTimestamp(sourceId);
-    % elseif sum(s ou rceStatus == 1) > 1
+    % elseif sum(sourceStatus == 1) > 1
     %     pcktCollisionCount = pcktCollisionCount + 1;
     %     sourceStatus  = sourceStatus + sourceBackoff;
     % end
--- sic.m
+++ sic.m
@@ -1,35 +1,34 @@
 function [outRandomAccessFrame,ackedPcktsCol,ackedPcktsRow] = sic(inRandomAccessFrame,nonCollPcktsCol,nonCollPcktsRow)
 % function [outRandomAccessFrame,ackedPcktsCol,ackedPcktsRow] = sic(inRandomAccessFrame,nonCollPcktsCol,nonCollPcktsRow)
-% 
+%
 % perform Successive Interference Cancellation (SIC) on a given Random Access Frame for Contention Resolution Diversity Slotted Aloha
-% 
+%
 % +++ Input parameters
 % 		- inRandomAccessFrame: the matrix containing slots and packets informations
-% 		- nonCollPcktsCol: an array containing the column indexes of packets that did not encounter collision (can include acked twins)
-% 		- nonCollPcktsRow: an array containing the row indexes of packets that did not encounter collision (can include acked twins)
-% 
+% 		- nonCollPcktsCol: an array containing the column indices of packets that did not encounter collision (can include acked twins)
+% 		- nonCollPcktsRow: an array containing the row indices of packets that did not encounter collision (can include acked twins)
+%
 % +++ Output parameters
 % 		- outRandomAccessFrame: the matrix containing slots and packets informations, after SIC
-% 		- ackedPcktsCol: an array containing the column indexes of acknowledged packets after SIC
-% 		- ackedPcktsRow: an array containing the row indexes of acknowledged packets after SIC
+% 		- ackedPcktsCol: an array containing the column indices of acknowledged packets after SIC
+% 		- ackedPcktsRow: an array containing the row indices of acknowledged packets after SIC
 
 nonCollPacketIdx = 1;
 while nonCollPacketIdx < numel(nonCollPcktsCol)
     twinPcktCol = inRandomAccessFrame( nonCollPcktsRow(nonCollPacketIdx),nonCollPcktsCol(nonCollPacketIdx) ); % get twin packet slot id
-
     if sum(inRandomAccessFrame(:,twinPcktCol)>0) > 1 % twin packet has collided
-        inRandomAccessFrame(nonCollPcktsRow(nonCollPacketIdx),twinPcktCol) = 0; % cancel twin packet, thus reducing interference
+        inRandomAccessFrame(nonCollPcktsRow(nonCollPacketIdx),twinPcktCol) = 0; % cancel interference
         if sum(inRandomAccessFrame(:,twinPcktCol)>0) == 1; % check if a new package can be acknowledged, thanks to interference cancellation
-            nonCollPcktsCol(numel(nonCollPcktsCol) + 1) = twinPcktCol; % update non collided packets col and row indexes arrays
+            nonCollPcktsCol(numel(nonCollPcktsCol) + 1) = twinPcktCol; % update non collided packets indices arrays
             nonCollPcktsRow(numel(nonCollPcktsRow) + 1) = find(inRandomAccessFrame(:,twinPcktCol));
         end
-    elseif sum(inRandomAccessFrame(:,twinPcktCol)>0) == 1 % twin packet has not collided, row and col indexes can be removed from the respective arrays
+    elseif sum(inRandomAccessFrame(:,twinPcktCol)>0) == 1 % twin packet has not collided, indices can be removed from the respective arrays
         nonCollTwinInd = find(nonCollPcktsCol == twinPcktCol);
         nonCollPcktsCol(nonCollTwinInd) = [];
         nonCollPcktsRow(nonCollTwinInd) = [];
     end
     nonCollPacketIdx = nonCollPacketIdx + 1;
-end    
+end
 outRandomAccessFrame = inRandomAccessFrame;
 ackedPcktsCol = nonCollPcktsCol;
 ackedPcktsRow = nonCollPcktsRow;
